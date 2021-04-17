88a522a3524b6ee899e5009a6dea17a5df0d3c5e
cfi <carlo@swiss.dev>
2021-04-17 13:36:44 +0200

    fixes #3 - use proper port

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -46,7 +46,7 @@ func InitialisePersistence(config *gorm.Config, models ...interface{}) (*gorm.DB
 func InitialisePersistenceFlags() {
 	// database flags
 	pflag.String("db.host", "localhost", "database hostname")
-	pflag.Int("db.port", 3306, "database port")
+	pflag.Int("db.port", 5432, "database port")
 	pflag.String("db.username", "root", "database username")
 	pflag.String("db.password", "", "database password")
 	pflag.String("db.database", "default", "database name")
